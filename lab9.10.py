2365706f7beed446bfca00780ea768e319c0a17e
dwayneellis <70253282+dwayneellis@users.noreply.github.com>
2020-10-07 12:04:39 -0500

    Add files via upload

---
+++ lab9.10.py
@@ -0,0 +1,24 @@
+# CIS 2348 Homework 1 Fall 2020.
+# Dwayne Ellis
+# Student ID: 0833810
+# Lab 9.10
+
+import csv
+
+fileName = input()
+
+wordsFrequency = {}
+
+with open(fileName, "r") as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        for word in row:
+            word = word.strip()
+            x = word.split(" ")
+            for stri in x:
+                if stri in wordsFrequency:
+                    wordsFrequency[stri] = wordsFrequency[stri] + 1
+                else:
+                    wordsFrequency[stri] = 1
+for key in wordsFrequency.keys():
+    print(key + " " + str(wordsFrequency[key]))
